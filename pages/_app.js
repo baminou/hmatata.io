6195755f5ca69d18fb72b12ba48fd0e425132a8f
Brice Aminou <brice.aminou@gmail.com>
2021-12-06 20:24:40 -0500

    fix

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
