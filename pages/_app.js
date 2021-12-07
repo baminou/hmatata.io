24eb6a53ca8eb23bf4d3a115d9878cb37dd47922
baminou <brice.aminou@gmail.com>
2021-12-07 01:36:14 +0000

    Deploying to gh-pages from @ baminou/hmatata.io@47101eedd328be03217719493b6462f2b8ffd87c 🚀

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
